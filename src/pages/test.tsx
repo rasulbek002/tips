dd87c37fd75472293104933bb0014b1f556bb84b
Rasul karimov <rasulbekkari008@gmail.com>
2023-10-26 16:30:57 +0600

    added test page

---
+++ src/pages/test.tsx
@@ -0,0 +1,13 @@
+import CreditCardForm from "@/entities/payments/card/CreditCard";
+import Positions from "@/entities/positions";
+import MainTitle from "@/shared/MainTitle";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="mb-5">
+        <CreditCardForm />
+      </div>
+    </div>
+  );
+}
